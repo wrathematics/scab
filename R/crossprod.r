b76bd36d52db07eff4b14beb0a3050b178cf3211
Drew Schmidt <wrathematics@gmail.com>
2019-05-21 06:40:02 -0400

    add R counterpart benchmark

---
+++ R/crossprod.r
@@ -0,0 +1,19 @@
+suppressMessages(library(pbdDMAT))
+init.grid(quiet=TRUE)
+comm.set.seed(1234, diff=TRUE)
+source("setup.r")
+
+n = 250
+m = as.integer((NUM_GPUS * GB_PER_GPU) * (BYTES_PER_VALUE / n))
+
+.pbd_env$BLDIM = c(BLOCKSIZE, BLOCKSIZE)
+.pbd_env$ICTXT = GRID_TYPE
+g = base.blacs(GRID_TYPE)
+
+t_gen = mpitime(x <- ddmatrix("runif", m, n))
+t_op = mpitime(crossprod(x))
+
+p = sprintf("crossprod,%d,%d,%d,%d,%d,%d,%d,%f,%f\n", comm.size(), g$NPROW, g$NPCOL, m, n, BLOCKSIZE, BLOCKSIZE, t_gen, t_op)
+comm.cat(p, quiet=TRUE)
+
+finalize()
---
+++ R/setup.r
@@ -0,0 +1,12 @@
+PROC_GRID_SQUARE = 0
+PROC_GRID_WIDE = 1
+PROC_GRID_TALL = 2
+
+mpitime = function(expr) pbdMPI::comm.timer(expr)[["max"]]
+
+# -----------------------------------------------------------------------------
+GRID_TYPE = PROC_GRID_TALL
+NUM_GPUS = 6
+GB_PER_GPU = 12
+BYTES_PER_VALUE = (1024*1024*1024/8)
+BLOCKSIZE = 16
